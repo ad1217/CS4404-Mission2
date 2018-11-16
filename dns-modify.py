158a9895bcde5c1487ab55f9d7f1a55ab15e0800
Adam Goldsmith <adam@adamgoldsmith.name>
2018-11-15 20:55:31 -0500

    Basic DNS rewrite

---
+++ dns-modify.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from pypacker import interceptor
+from pypacker.layer3 import ip
+from pypacker.layer4 import udp
+from pypacker.layer567 import dns
+
+TARGET_HOSTNAMES = ['nonbast.com.']
+OUR_SERVER = bytes(int(x) for x in '10.4.16.1'.split('.'))
+
+def verdict_cb(ll_data, ll_proto_id, data, ctx):
+    ip1 = ip.IP(data)
+    if dns.DNS in ip1:
+        dns1 = ip1[dns.DNS]
+
+        for answer in dns1.answers:
+            if answer.type == dns.DNS_A:
+                answer.address = OUR_SERVER
+
+        # force recalculation of UDP checksum, seems to be a bug in pypacker
+        ip1[udp.UDP]._calc_sum()
+
+    return ip1.bin(True), interceptor.NF_ACCEPT
+
+ictor = interceptor.Interceptor()
+try:
+    ictor.start(verdict_cb, queue_ids=[0, 1, 2])
+except KeyboardInterrupt:
+    ictor.stop()
